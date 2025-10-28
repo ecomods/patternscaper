5d224012bc295863fcd814290ad769fa7893a20b
Selina Baldauf <selina.baldauf@gmx.de>
2025-10-28 13:08:32 +0100

    Update workflow2 to new functions and data types

--- inst/examples/example2_selforganized.R
+++ inst/examples/example2_selforganized.R
@@ -8,18 +8,45 @@ devtools::load_all()
 #--------------------------------------------------------------------
 
 # only those types that refer to self-organized ecotones
-bare <- create_landscape_random(tree_prop = 0.01, seed = 42)
-spots <- create_landscape_spots(
-  n_spots = 12, spot_radius = 8, noise_radius_sd = 0.2, regular_spots = T, seed = 42
+bare <- create_landscape(
+  pattern = "random",
+  custom_pattern = "bare", # Change pattern from random to bare
+  tree_prop = 0.01,
+  seed = 42
 )
-labyrith <- create_landscape_labyrinth(
-  band_fuzziness=0.01, seed = 42
+spots <- create_landscape(
+  pattern = "spots",
+  n_spots = 12,
+  spot_radius = 8,
+  noise_radius_sd = 0.2,
+  regular_spots = TRUE,
+  seed = 42
 )
-gaps <- create_landscape_gaps(
-  n_spots = 10, spot_radius = 8, seed = 42, regular_spots = T
+labyrinth <- create_landscape(
+  pattern = "labyrinth",
+  band_fuzziness = 0.01,
+  seed = 42
 )
-dense <- create_landscape_random(tree_prop = 0.99, seed = 42)
-all_examples <- list(bare,spots,labyrith,gaps,dense)
-types = c("bare","spots","labyrinth","gaps","dense")
-plot_landscape_list(all_examples,titles=types,show_legend = F, ncol=length(all_examples))
 
+gaps <- create_landscape(
+  pattern = "gaps",
+  n_spots = 10,
+  spot_radius = 8,
+  seed = 42,
+  regular_spots = TRUE
+)
+
+dense <- create_landscape(
+  pattern = "random",
+  custom_pattern = "dense",
+  tree_prop = 0.99,
+  seed = 42
+)
+
+all_examples <- list(bare, spots, labyrinth, gaps, dense)
+
+plot_landscape_list(
+  all_examples,
+  show_legend = FALSE,
+  ncol = length(all_examples)
+)
