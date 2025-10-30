4a376d823636161e91a02c63f8b39b925de416ea
btietjen <britta.tietjen@fu-berlin.de>
2025-10-30 17:13:00 +0100

    some naming corrections
    #BRITTA highlights important to dos

--- R/nn_neuralnet.R
+++ R/nn_neuralnet.R
@@ -84,14 +84,14 @@ train_nn_neuralnet <- function(
   class_names <- levels(metrics_scaled$pattern)
 
   # train a network
-  model_ecotones_nn <- neuralnet::neuralnet(
+  nn_model <- neuralnet::neuralnet(
     formula = pattern ~ .,
     data = metrics_scaled,
     hidden = hidden_layers
   )
 
   # return model object
-  model_ecotones_nn
+  nn_model
 
 }
 
@@ -112,7 +112,6 @@ test_nn_neuralnet <- function(
     nn_model = NULL
 ) {
 
-
   # subset selected metrics if provided
   if (!is.null(metrics_selected)) {
     # Subset only the selected metrics
@@ -120,11 +119,11 @@ test_nn_neuralnet <- function(
   }
 
   # Filter out NA values and warn the user about how many were removed
-  metrics_na_test <- subset(test_metrics, is.na(value))
-  if (nrow(metrics_na) > 0) {
+  test_metrics_na <- subset(test_metrics, is.na(value))
+  if (nrow(test_metrics_na) > 0) {
     warning(paste(
       "Removed",
-      nrow(metrics_na),
+      nrow(test_metrics_na),
       "metrics with NA values (Check the \"value\" column in your metrics data for details)"
     ))
   }
@@ -174,7 +173,7 @@ test_nn_neuralnet <- function(
   # Store the class names
   test_class_names <- levels(test_metrics_scaled$pattern)
 
-  pred <- predict(model_ecotones_nn, test_metrics_scaled)
+  pred <- predict(nn_model, test_metrics_scaled)
   labels <- ecotone_types
   prediction_label <- data.frame(max.col(pred)) %>%
     mutate(pred=labels[max.col.pred.]) %>% select(2) %>% unlist()
@@ -187,3 +186,95 @@ test_nn_neuralnet <- function(
 
 
 }
+
+#' Apply a multi-layer Neural Network for Landscape Classification
+#'
+#' Applies a multi-layer neural network model to classify landscapes
+#' based on metrics. Uses neuralnet package.
+#'
+#' @param metrics tibble. Metrics from calculate_landscape_metrics().
+#' @param metrics_selected Character vector. Names of metrics to use as features.
+#' @param nn_model neural network model. Result from train_nn_neuralnet()
+#'
+#' @export
+apply_nn_neuralnet <- function(
+    test_metrics = NULL,
+    metrics_selected = NULL,
+    nn_model = NULL
+) {
+
+
+  # subset selected metrics if provided
+  if (!is.null(metrics_selected)) {
+    # Subset only the selected metrics
+    test_metrics <- subset(test_metrics, metric %in% metrics_selected)
+  }
+
+  # Filter out NA values and warn the user about how many were removed
+  test_metrics_na <- subset(test_metrics, is.na(value))
+  if (nrow(test_metrics_na) > 0) {
+    warning(paste(
+      "Removed",
+      nrow(metrics_na),
+      "metrics with NA values (Check the \"value\" column in your metrics data for details)"
+    ))
+  }
+  # Remove the missing values
+  test_metrics <- subset(test_metrics, !is.na(value))
+
+  # extract the level of metrics so it can be accessed later
+  test_metric_levels <- unique(test_metrics$level)
+
+  # if needed, add info on class and patch id to the metric name
+  # this is needed when the metric is calculated not on the landscape level,
+  # but on the class or patch level
+  test_metrics <- test_metrics |>
+    dplyr::mutate(
+      metric = stringr::str_remove(
+        paste0(metric, "_", class, "_", id),
+        "_NA_NA"
+      )
+    ) |>
+    dplyr::select(metric, value, landscape_name)
+
+  # Reformat the table to wide format
+  test_metrics_wide <-test_metrics |>
+    tidyr::pivot_wider(
+      names_from = metric,
+      values_from = value
+    ) |>
+    dplyr::select(-landscape_name)
+
+  # Normalize the predictor variables (all columns except for the pattern column)
+  test_predictors <- test_metrics_wide
+
+#BRITTA: HIER ENTSTEHT NOCH EIN PROBLEM
+#DIE WERDEN ALLE AUF 0 GESCALED
+  test_predictors_scaled <- scale(test_predictors)
+
+  # Store scaling parameters for future use
+  # This will be used to scale new data before prediction
+  test_scaling_params <- list(
+    center = attr(test_predictors_scaled, "scaled:center"),
+    scale = attr(test_predictors_scaled, "scaled:scale")
+  )
+
+  # Combine the scaled predictors with the target variable
+  test_metrics_scaled <- data.frame(
+    test_predictors_scaled
+  )
+
+#BRITTA: um das scaling Problem zu umgehen, habe ich
+#hier statt test_metrics_scaled die nicht skalierten
+#Daten test_predictors eingegeben
+    pred <- predict(nn_model, test_predictors)
+#  labels <- ecotone_types
+#  prediction_label <- data.frame(max.col(pred)) %>%
+#    mutate(pred=labels[max.col.pred.]) %>% select(2) %>% unlist()
+
+  names(pred) <- ecotone_types
+
+  pred
+
+}
+
--- inst/examples/test_neuralnet_BT.R
+++ inst/examples/test_neuralnet_BT.R
@@ -7,6 +7,9 @@ devtools::load_all()
 library("neuralnet")
 library("tidyverse")
 
+
+#https://www.datacamp.com/tutorial/neural-network-models-r
+
 #--------------------------------------------------------------------
 # General landscape types and their titles
 #--------------------------------------------------------------------
@@ -27,29 +30,45 @@ n_ecotones <- length(ecotone_types)
 #----------------------------------------------------------
 #generate all training landscapes
 training_landscapes <- create_training_landscapes(
-  n = 30,
+  n = 100,
   seed = 42,
   patterns = ecotone_types
 )
 
 # calculate landscape metrics on the landscape level
-landscape_metrics <- calculate_landscape_metrics(
-  ecotone_landscapes,
+training_metrics <- calculate_landscape_metrics(
+  training_landscapes,
   level = "landscape"
 )
 
 # find the 10 best metrics based on coefficient of variation
 best_10 <- evaluate_landscape_metrics(
-  calculated_metrics = landscape_metrics,
-  method = "lin_mod_r2",
+  metrics = training_metrics,
+  method = "coeffvar_all",
   metrics_number = 10
 )
+
+#----------------------------------------------------------
+# Train neural network model
+#----------------------------------------------------------
+
+# train a network
+model_neuralnet <- train_nn_neuralnet(
+  metrics = training_metrics,
+  metrics_selected = best_10,
+  hidden_layers = c(5,5),
+  seed = 42
+)
+
+# look at the model object
+#model_neuralnet
+plot(model_neuralnet,rep = "best")
 
 #----------------------------------------------------------
 #test landscapes and their metrics (only 10 best)
 #----------------------------------------------------------
 test_landscapes <- create_training_landscapes(
-  n = 30,
+  n = 100,
   seed = 100,
   patterns = ecotone_types
 )
@@ -61,26 +80,68 @@ test_metrics <- calculate_landscape_metrics(
   level = "landscape"
 )
 
-
-# train a network
-model_ecotones_nn <- train_nn_neuralnet(
-  metrics = test_metrics,
-  metrics_selected = best_10,
-  hidden_layers = c(5,5),
-  seed = 42
-)
-
-# look at the model object
-model_ecotones_nn
-plot(model_ecotones_nn,rep = "best")
-
 #--------------------------------------------------------------------
 # Test neural network model
 #--------------------------------------------------------------------
 
+
+#BRITTA: Achtung - Reihenfolge der Beschriftungen stimmt nicht
+#mit Reihenfolge der Einträge überein
 test_nn_neuralnet(
   test_metrics = test_metrics,
   metrics_selected = best_10,
-  nn_model = model_ecotones_nn
+  nn_model = model_neuralnet
+)
+
+
+#--------------------------------------------------------------------
+# Test picture
+#--------------------------------------------------------------------
+
+# Read in the satellite image
+pic_dir <- "inst/examples/Ecotone/" #folder name
+pic_names <- list.files(pic_dir) #file names
+pic_names
+
+i <- 1
+image <- terra::rast(paste(pic_dir, pic_names[i], sep = ""))
+# If it's a multi-band image
+band1 <- image[[1]]
+# Apply threshold
+binary_class <- band1 < 70 #this parameter determines the sensitivity towards
+#classification as vegetation - the higher the more vegetation
+test_matrix <- as.matrix(binary_class, wide = TRUE)
+test_raster <- terra::rast(test_matrix)
+
+#test plotting of binary categorization
+par(mfrow = c(1, 2), pty = "s")
+raster::plot(
+  raster::flip(band1),
+  col = terrain.colors(25),
+  main = "Initial Landscape"
+)
+raster::plot(
+  raster::flip(test_raster),
+  col = c(terrain.colors(25)[25], terrain.colors(25)[1]),
+  main = "Binary Landscape"
+)
+par(mfrow = c(1, 1))
+
+# Bring the test raster in the right format for package functions
+test_raster_l <- landscape(data = test_raster, name = "test raster")
+
+# calculate selected landscape metrics on the landscape level
+test_metrics_pic <- calculate_landscape_metrics(
+  test_raster_l,
+  metrics = best_10,
+  level = "landscape"
+)
+
+
+#apply the neural network model to the picture
+apply_nn_neuralnet(
+  test_metrics = test_metrics_pic,
+  metrics_selected = best_10,
+  nn_model = model_neuralnet
 )
 
