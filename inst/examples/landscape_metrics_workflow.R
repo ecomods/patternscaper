1f01e03bc491440cdb67fb639d097968b3947dc4
Selina Baldauf <selina.baldauf@fu-berlin.de>
2025-11-25 18:03:20 +0100

    Simplify workflow script

--- inst/examples/landscape_metrics_workflow.R
+++ inst/examples/landscape_metrics_workflow.R
@@ -37,44 +37,24 @@ landscape_metrics <- calculate_landscape_metrics(
 )
 
 # find the 10 best metrics based on coefficient of variation
+# Verbose = TRUE will print details on which metrics are excluded because of which reasons
 best_10 <- evaluate_landscape_metrics(
   metrics = landscape_metrics,
   metrics_number = 10,
   verbose = TRUE
 )
 
-# Find best 10 based on linear model R-squared
-best_10_linmod_r2 <- evaluate_landscape_metrics(
+# There are different methods to select the best metrics
+best_10 <- evaluate_landscape_metrics(
   metrics = landscape_metrics,
-  method = "lin_mod_r2",
-  metrics_number = 10
-)
-
-# Find best 10 based on difference from group mean
-best_10_group_diff <- evaluate_landscape_metrics(
-  metrics = landscape_metrics,
-  method = "mean_groups",
-  metrics_number = 10
-)
-
-# Find best 10 based on Fisher score
-best_10_fisher <- evaluate_landscape_metrics(
-  metrics = landscape_metrics,
-  method = "fisher_score",
-  metrics_number = 10
-)
-
-# Find best 10 based on Fisher score
-best_10_kruskal <- evaluate_landscape_metrics(
-  metrics = landscape_metrics,
-  method = "kruskal_p",
+  method = "mean_groups", # one of: "coeffvar_all", "lin_mod_r2", "mean_groups", "fisher_score", "kruskal_p"
   metrics_number = 10
 )
 
 # Plot the 10 best metrics
 plot_metrics(
   metrics = landscape_metrics,
-  selected_metrics = best_10_group_diff
+  selected_metrics = best_10
 )
 
 # Train a network -----------------------------------------------
@@ -82,7 +62,7 @@ plot_metrics(
 # warning will tell you that folds need to be reduced to 2
 model <- train_nn_metrics(
   metrics = landscape_metrics,
-  metrics_selected = best_10_group_diff,
+  metrics_selected = best_10,
   cv_method = "k-fold"
 )
 
