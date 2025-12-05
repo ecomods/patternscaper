379e72c4bb98ca00e87a5063e681271d6f5f61ad
selinaZitrone <selina.baldauf@fu-berlin.de>
2025-12-05 16:21:16 +0100

    rename shell script

---
+++ inst/HPC/selforg.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+#SBATCH --job-name=systematic_test_neuralnet_selforg
+#SBATCH --mail-user=ujcuz@zedat.fu-berlin.de
+#SBATCH --mail-type=end
+#SBATCH --nodes=1
+#SBATCH -c 4
+#SBATCH --ntasks=1
+#SBATCH --mem=50G
+#SBATCH --time=24:00:00
+#SBATCH --qos=standard
+
+module add R
+
+Rscript "systematic_test_neuralnet_selforg.R" 
