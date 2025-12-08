34199bec60b2c14070033e6488027c12d3f1fd4a
Selina Baldauf <selina.baldauf@gmx.de>
2025-12-08 15:55:59 +0100

    Add job scripts

---
+++ inst/HPC/keras_selforg.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+#SBATCH --job-name=keras_selforg
+#SBATCH --mail-user=ujcuz@zedat.fu-berlin.de
+#SBATCH --mail-type=end
+#SBATCH --nodes=1
+#SBATCH -c 1
+#SBATCH --ntasks=1
+#SBATCH --mem=50G
+#SBATCH --time=48:00:00
+#SBATCH --qos=standard
+
+module add R
+
+# Run the R script with arguments
+Rscript "systematic_test_keras.R" selforg results_selforg
