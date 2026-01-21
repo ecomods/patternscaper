0f810f56be573f7f75eb9268967ee8012800bd2a
Selina Baldauf <selina.baldauf@fu-berlin.de>
2026-01-21 21:55:19 +0100

    Update times for shell script test on HPC

--- inst/HPC/neuralnet_ecotones.sh
+++ inst/HPC/neuralnet_ecotones.sh
@@ -6,10 +6,10 @@
 #SBATCH --nodes=1
 #SBATCH -c 4
 #SBATCH --ntasks=1
-#SBATCH --mem=10G
-#SBATCH --time=10:00:00
+#SBATCH --mem=1G
+#SBATCH --time=00:05:00
 #SBATCH --qos=standard
-#SBATCH --output=nn_ecotones_%j.out
+#SBATCH --output=nn_ecotones_test_%j.out
 
 module add GDAL
 module add R
